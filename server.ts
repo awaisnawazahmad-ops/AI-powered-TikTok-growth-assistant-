bf7698b505f0def07da468360f3bfff37cdc1db6
awaisnawazahmad-ops <awaisnawazahmad@gmail.com>
2026-07-26 12:35:21 +0500

    Update server.ts

--- server.ts
+++ server.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import path from "path";
 import dotenv from "dotenv";
-import { createServer as createViteServer } from "vite";
 import { GoogleGenAI } from "@google/genai";
 
 dotenv.config();
